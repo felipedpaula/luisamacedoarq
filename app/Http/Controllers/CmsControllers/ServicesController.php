f3462067015caf3097ab1b158eaf4dcd5f4958bc
Felipe Palmeira <felipedpaula97@gmail.com>
2023-11-30 20:49:05 -0300

    Serviços storage

--- app/Http/Controllers/CmsControllers/ServicesController.php
+++ app/Http/Controllers/CmsControllers/ServicesController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\CmsControllers;
 use App\Http\Controllers\Controller;
 use App\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class ServicesController extends Controller
 {
@@ -26,5 +28,57 @@ class ServicesController extends Controller
     public function register() {
         $this->dadosPagina['tituloPagina'] = 'Novo Serviço';
         return view('cms.pages.services.register', $this->dadosPagina);
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = $request->only([
+            'title',
+            'resume',
+            'body',
+            'img_default',
+            'status',
+        ]);
+
+        $rules = [
+            'title' => ['required', 'string', 'max:255'],
+            'resume' => ['required', 'string'],
+            'body' => ['required', 'string'],
+            'status' => ['required', 'in:0,1'],
+        ];
+
+        if($request->file('img_default')){
+            $img_default = $request->file('img_default')->store('public/images/services');
+            $url = asset(Storage::url($img_default));
+            $data['img_default']= Storage::url($url);
+
+        }else{
+            $data['img_default']= null ;
+        }
+
+        $validator = Validator::make($data, $rules);
+        if($validator->fails()) {
+            return redirect()->route('admin.content.create')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        try {
+            $project = new Service([
+                'title' => $data['title'],
+                'resume' =>  $data['resume'],
+                'body' =>  $data['body'],
+                'status' => $data['status'],
+                'img_default' => $data['img_default']
+            ]);
+
+            $project->save();
+
+            return redirect()->route('admin.services.index')->with('success', 'Serviço criado com sucesso!');
+
+        } catch (\Exception $e) {
+            return redirect()->route('admin.service.create')->with('errors', 'Ocorreu um erro ao criar o Serviço. Por favor, tente novamente.');
+        }
     }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title',
+        'resume',
+        'body',
+        'img_default',
+        'status',
+    ];
+
 }
